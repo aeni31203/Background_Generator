7e9928c023188ce36624617c86000b4720618011
Aeni <aeni31203@gmail.com>
2023-03-22 01:31:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// let tablePara = document.getElementById("table");
+// let tableBtn = document.getElementById("btn");
+
+// tableBtn.addEventListener("click", tableGenerator);
+
+// function tableGenerator() {
+//   function tableLoop() {
+//     let number = prompt("enter a value");
+//     // let tableInput = document.getElementById("input").value;
+
+//     for (let i = 1; i <= 10; i++) {
+//         tablePara.innerHTML += `${number} X ${i} = ${number * i} <br>`;
+//     //    = getTable + "<br>"
+//     //   document.write(getTable + "<br>");
+//     }
+//   }
+//   tableLoop();
+// }
+
+// const arr = [1,2,4,5,6,7]
+
+// const newArr =arr.forEach((num)=>{
+// console.log(num*2);
+// })
+
+// const mapedArr = arr.map((elem)=>{
+// return elem*3
+// })
+// console.log(mapedArr);
+
+// const filteredArr = mapedArr.filter(num =>num % 2 === 0)
+// console.log(filteredArr);
+
+// const reducedArr = arr.reduce((acc, num)=>{
+//     return acc + num
+// },1)
+// console.log(reducedArr);
