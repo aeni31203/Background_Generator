52940b23732a8d3b0396ca2cea48dbab2442858a
Aeni <aeni31203@gmail.com>
2023-03-22 02:23:23 -0700

    updated css

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-// let tablePara = document.getElementById("table");
-// let tableBtn = document.getElementById("btn");
+let input1 = document.getElementById("color1");
+let input2 = document.getElementById("color2");
+let body = document.getElementById("colorTheme");
+let para = document.getElementById("para");
 
-// tableBtn.addEventListener("click", tableGenerator);
+function colorGenerator() {
+  body.style.background = `linear-gradient(to right,${input1.value},${input2.value})`;
+  para.textContent = body.style.background + ";";
+}
 
-// function tableGenerator() {
-//   function tableLoop() {
-//     let number = prompt("enter a value");
-//     // let tableInput = document.getElementById("input").value;
-
-//     for (let i = 1; i <= 10; i++) {
-//         tablePara.innerHTML += `${number} X ${i} = ${number * i} <br>`;
-//     //    = getTable + "<br>"
-//     //   document.write(getTable + "<br>");
-//     }
-//   }
-//   tableLoop();
-// }
-
-// const arr = [1,2,4,5,6,7]
-
-// const newArr =arr.forEach((num)=>{
-// console.log(num*2);
-// })
-
-// const mapedArr = arr.map((elem)=>{
-// return elem*3
-// })
-// console.log(mapedArr);
-
-// const filteredArr = mapedArr.filter(num =>num % 2 === 0)
-// console.log(filteredArr);
-
-// const reducedArr = arr.reduce((acc, num)=>{
-//     return acc + num
-// },1)
-// console.log(reducedArr);
+input1.addEventListener("input", colorGenerator);
+input2.addEventListener("input", colorGenerator);
